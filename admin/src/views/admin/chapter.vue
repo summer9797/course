32f48c0ceb950a62e99402c4ac95f5d273cbabce
summer9797 <1145091226@qq.com>
2020-10-21 22:30:28 +0800

    分页组件完整版

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,81 +1,96 @@
 <template>
+    <div>
+        <p>
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+                <i class="ace-icon fa fa-refresh red2"></i>
+                refresh
+            </button>
+        </p>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>名称</th>
-            <th>课程</th>
-            <th>操作</th>
-        </tr>
-        </thead>
+        <pagination ref="pagination" v-bind:list="list"></pagination>
 
-        <tbody>
-        <tr v-for="chapter in chapters" v-bind:key="chapter">
-            <td>{{chapter.id}}</td>
-            <td>{{chapter.name}}</td>
-            <td>{{chapter.courseId}}</td>
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                    <button class="btn btn-xs btn-success">
-                        <i class="ace-icon fa fa-check bigger-120"></i>
-                    </button>
+        <table id="simple-table" class="table  table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>名称</th>
+                <th>课程</th>
+                <th>操作</th>
+            </tr>
+            </thead>
 
-                    <button class="btn btn-xs btn-info">
-                        <i class="ace-icon fa fa-pencil bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-danger">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-warning">
-                        <i class="ace-icon fa fa-flag bigger-120"></i>
-                    </button>
-                </div>
-
-                <div class="hidden-md hidden-lg">
-                    <div class="inline pos-rel">
-                        <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                            <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+            <tbody>
+            <tr v-for="chapter in chapters" v-bind:key="chapter">
+                <td>{{chapter.id}}</td>
+                <td>{{chapter.name}}</td>
+                <td>{{chapter.courseId}}</td>
+                <td>
+                    <div class="hidden-sm hidden-xs btn-group">
+                        <button class="btn btn-xs btn-success">
+                            <i class="ace-icon fa fa-check bigger-120"></i>
                         </button>
 
-                        <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                            <li>
-                                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+                        <button class="btn btn-xs btn-info">
+                            <i class="ace-icon fa fa-pencil bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-danger">
+                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-warning">
+                            <i class="ace-icon fa fa-flag bigger-120"></i>
+                        </button>
+                    </div>
+
+                    <div class="hidden-md hidden-lg">
+                        <div class="inline pos-rel">
+                            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                                <li>
+                                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                                <li>
+                                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                                <li>
+                                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                                </a>
-                            </li>
-                        </ul>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
 
 </template>
 
 <script>
+    import Pagination from "../../components/pagination";
     export default {
+        components:{
+            Pagination
+        },
         name: "chapter",
         data: function(){
             return{
@@ -84,16 +99,19 @@
         },
         mounted: function(){
             let _this = this;
-            _this.list();
+            _this.list(1);
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
+                console.log(page, _this.$refs.pagination.size),
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-                    page:1, size:1
+                    page: page,
+                    size: _this.$refs.pagination.size
                 }).then((response)=>{
                     console.log(response);
                     _this.chapters = response.data.list;
+                    _this.$refs.pagination.render(page, response.data.total)
                 })
             }
         }
